70b39cd052686435a74696f797fe91c2edf13ae0
ashtonlim <ashtonlim22@gmail.com>
2021-05-24 10:55:45 +0800

    setup 2.1 add reporter

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,14 @@
 module.exports = {
     preset: "jest-playwright-preset",
-    setupFilesAfterEnv: ['./jest.setup.js']
+    setupFilesAfterEnv: ['./jest.setup.js'],
+    "reporters": [
+        "default",
+        "jest-html-reporters"
+    ],
+    // reporters: [
+    //     "default",
+    //     ["./node_modules/jest-html-reporter", {
+    //         "pageTitle": "Test Report"
+    //     }]
+    // ]
 }
--- jest.setup.js
+++ jest.setup.js
@@ -1,4 +1,4 @@
-const secs = 40
+const secs = 60
 jest.setTimeout(secs*1000)
 
 // --viewport-size=1920,1080
