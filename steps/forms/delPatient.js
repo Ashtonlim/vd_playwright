7767dc5a57945aa1bf7808fe7fd2c3474c0bc4af
ashtonlim <ashtonlim22@gmail.com>
2021-06-11 10:04:55 +0800

    save

--- steps/forms/delPatient.js
+++ steps/forms/delPatient.js
@@ -1,19 +1,18 @@
 module.exports.delPatient = async (page, { id, pName, nric, num }) => {
-    console.log(await page.isVisible(`text=${id} ${nric} ${pName} +65${num} Active Preview >> button`))
-    if ((await page.isVisible(`text=${id} ${nric} ${pName} +65${num} Active Preview >> button`)) === false) {
-        await page.goto('https://hub-staging.vaultdragon.com/patient/list')
-        await page.waitForTimeout(1000)
-    }
+  console.log(await page.isVisible(`text=${id} ${nric} ${pName} +65${num} Active Preview >> button`))
+  if ((await page.isVisible(`text=${id} ${nric} ${pName} +65${num} Active Preview >> button`)) === false) {
+    await page.goto('https://hub-staging.vaultdragon.com/patient/list')
+    await page.waitForTimeout(1000)
+  }
 
-    await page.click(`text=${id} ${nric} ${pName} +65${num} Active Preview >> button`)
-    await page.click(`text=${pName}`)
-    await page.click('#edit-patient-profile')
-    await page.click('text=Personal Information')
-    await page.click('text=Delete')
-    await page.click(`[placeholder='${id}']`)
+  await page.click(`text=${id} ${nric} ${pName} +65${num} Active Preview >> button`)
+  await page.click(`text=${pName}`)
+  await page.click('#edit-patient-profile')
+  await page.click('text=Personal Information')
+  await page.click('text=Delete')
+  await page.click(`[placeholder='${id}']`)
 
-    await page.fill(`[placeholder='${id}']`, `${id}`)
-    await page.click('button:has-text("Ok")')
-    await page.isVisible('Patient deleted successfully')
-    await page.waitForTimeout(2000)
+  await page.fill(`[placeholder='${id}']`, `${id}`)
+  await page.click('button:has-text("Ok")')
+  await page.waitForSelector('text=Patient deleted successfully')
 }
