1b9c6e75aae528109e877683d499959ea625aeb0
ashtonlim <ashtonlim22@gmail.com>
2021-06-25 11:33:01 +0800

    run

--- tests/prod1/jest.config.js
+++ tests/prod1/jest.config.js
@@ -1,5 +1,8 @@
 const path = require('path')
+
 const { reportProdConfig } = require(process.cwd() + '/g')
+// get the name of folder, to produce same html filename, i.e. prodx.html
 const reportFilename = path.dirname(__filename).split(path.sep).pop()
 reportProdConfig['reporters'][1][1]['filename'] = `${reportFilename}.html`
+
 module.exports = reportProdConfig
